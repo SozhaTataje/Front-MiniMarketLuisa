0fed40c0af4788970a2145fc08c9c72122dbb3f6
MarioHQE <166763888+MarioHQE@users.noreply.github.com>
2025-07-20 17:29:20 -0500

    Configuracion de viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://desarrollowebintegrado-13w4.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      },
-      '/producto': {
-        target: 'https://desarrollowebintegrado-13w4.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+ 
 });
 
