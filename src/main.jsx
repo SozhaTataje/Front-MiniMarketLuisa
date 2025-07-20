feb0413a646833fa6aa9ca9f25e3f00462285b82
MarioHQE <166763888+MarioHQE@users.noreply.github.com>
2025-07-20 18:08:22 -0500

    Cambios

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./index.css";
 import { CartProvider } from "./context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </React.StrictMode>
+  <CartProvider>
+    <App />
+  </CartProvider>
 );
